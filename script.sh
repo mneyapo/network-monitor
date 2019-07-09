4de2c7c0944471229305b17f4df2e79711d4b840
Maroiane Nasrellah <39185579+mneyapo@users.noreply.github.com>
2019-07-09 15:05:21 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir ~/bin
+
+cd ~/bin
+
+git clone https://github.com/mneyapo/network-monitor.git
+
+cd network-monitor
+
+sudo mv network-monitor.sh ..
+
+cd ..
+
+ls
+
+sudo rm -r network-monitor
+
+sudo chmod +x ~/bin/network-monitor.sh
+
+cd
+
+source ~/.bashrc
+
+echo "*/5 * * * * root bash /home/pi/bin/network-monitor.sh"  | sudo tee -a /etc/crontab
+
+cat /etc/crontab
